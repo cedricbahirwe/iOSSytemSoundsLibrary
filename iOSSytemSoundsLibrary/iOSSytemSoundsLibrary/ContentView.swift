91149935912fe2bf8cfc82a7742eaed2f0303e96
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-19 23:25:19 +0200

    Add Full Project

---
+++ iOSSytemSoundsLibrary/iOSSytemSoundsLibrary/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  iOSSytemSoundsLibrary
+//
+//  Created by Cédric Bahirwe on 19/02/2021.
+//
+
+import SwiftUI
+import AudioToolbox
+
+
+struct ContentView: View {
+    @State private var audioFileList: [NSURL] = []
+    var body: some View {
+        NavigationView {
+            List(audioFileList.compactMap{ $0 }, id: \.self) { audioFile in
+                if let title = audioFile.lastPathComponent {
+                    Text(title)
+                        .font(.system(size: 20))
+                        .fontWeight(.medium)
+                        .onTapGesture {
+                            play(at: audioFile)
+                        }
+                }
+            }
+            .navigationBarTitle("\(audioFileList.count) System Sounds")
+            .redacted(reason: audioFileList.count <= 0 ? .placeholder : [])
+            .onAppear(perform: loadAudioFileList)
+        }
+        
+    }
+    
+    
+    /// Play a specified sound for a given url
+    /// - Parameter url: the path of the sound to be played
+    private func play(at url: NSURL) {
+        var soundID = SystemSoundID();
+        AudioServicesCreateSystemSoundID(url, &soundID)
+        AudioServicesPlaySystemSound(soundID);
+    }
+    
+    /// Fetch and Load audio file find in the specified directory
+    private func loadAudioFileList() {
+        let fileManager = FileManager.default
+        if let enumerator:FileManager.DirectoryEnumerator = fileManager.enumerator(atPath: "/System/Library/Audio/UISounds") {
+            while let element = enumerator.nextObject() as? String {
+                // Check if  element has caf (Core audio file) extension
+                if element.hasSuffix("caf")  {
+                    let prefix = "file:///System/Library/Audio/UISounds/"
+                    let link = NSURL(string: "\(prefix+element)")!
+                    print(link)
+                    audioFileList.append(link)
+                    
+                }
+            }
+            // Since we are sure that every NSURl which is added to the list
+            // should have a caf extension meaning that the "lastPathComponent"
+            // of NSURl will not be "nil" so we're using force unwrap for sorting
+            audioFileList.sort(by: { $0.lastPathComponent! < $1.lastPathComponent!})
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSSytemSoundsLibrary/iOSSytemSoundsLibrary/iOSSytemSoundsLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSytemSoundsLibraryApp.swift
+//  iOSSytemSoundsLibrary
+//
+//  Created by Cédric Bahirwe on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSSytemSoundsLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
